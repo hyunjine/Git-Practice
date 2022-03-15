98c3cb3b6f1183cb897d7f98fb83dd09e4f2dcec
hyunjine <hyunjin_98_dev@naver.com>
2022-03-15 23:09:30 +0900

    Test2 commit

--- Kotlin_Practice/app/src/main/java/com/hyunjine/kotlin_practice/ScopingFunction.kt
+++ Kotlin_Practice/app/src/main/java/com/hyunjine/kotlin_practice/ScopingFunction.kt
@@ -3,7 +3,11 @@ package com.hyunjine.kotlin_practice
 import android.util.Log
 
 class ScopingFunction {
-    private fun Test() {
+    private fun Test_1() {
         Log.d("tag", "Test: gdgd")
+    }
+
+    private fun Test_2() {
+
     }
 }
